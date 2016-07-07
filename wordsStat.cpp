a28f80980f83da460b430a38884ede0b964b614a
iaro <iaroslava.bezshyiko@cern.ch>
2016-07-07 17:24:25 +0200

    added functions which will sort data by its value

--- wordsStat.cpp
+++ wordsStat.cpp
@@ -5,9 +5,45 @@
 #include <vector>
 #include <map>
 #include <stdio.h>
+#include <algorithm>	// for sort() function
 typedef std::map<std::string, int> StrIntMap;
+typedef std::pair<std::string, int> PairVect;
 
 using namespace std;
+
+
+//we create template class. That means we should not define some specific type as input for function. but when we call this class (structure) we should define in <> brackets which type of inputs will be there.
+template<class T>
+struct less_second : std::binary_function<T,T,bool>
+{
+	inline bool operator()(const T& lhs,const T& rhs)
+	{
+		return lhs.second > rhs.second;
+	}
+};
+
+
+void PrintSorted( const std::vector<PairVect>& vec )
+{
+    std::vector<PairVect>::const_iterator vec_it = vec.begin();
+    std::cout << std::endl;
+    for ( ; vec_it != vec.end(); vec_it++ )
+    {
+        std::string s = (*vec_it).first;
+        double weight = (*vec_it).second;
+        std::cout << s << " "
+                  << weight   << std::endl;
+    }
+}
+
+vector<PairVect > sort_by_weight(StrIntMap& s_map)
+{
+	std::vector<PairVect> vec(s_map.begin(), s_map.end());
+	std::sort(vec.begin(),vec.end(),less_second<PairVect>());
+	PrintSorted(vec);
+	return vec;
+}
+
 
 void noCharact(string str, vector<string>& vect){
 	vect.clear();
@@ -59,7 +95,7 @@ int main(){
 	}
 	else cout<<"Unable to open file" <<endl;
 	bookFile.close();
-	
+	sort_by_weight(dictionary);
 	for(StrIntMap::iterator it = dictionary.begin(); it!=dictionary.end(); ++it){
 		cout<<it->first<<" occured "<<it->second<<" times. \n";
 	}
